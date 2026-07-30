5982db536c1e4e699d5cfffb8f858acbf2084c29
Subhash Pandit <subhashpandit@Subhashs-MacBook-Pro.local>
2026-07-31 00:05:42 +0530

    First commit

---
+++ src/test/java/com/automationexcellence/SampleTest.java
@@ -0,0 +1,40 @@
+package com.automationexcellence;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+    
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        // Headless arguments are critical for Jenkins execution
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void verifyGoogleTitle() {
+        driver.get("https://google.com");
+        String title = driver.getTitle();
+        System.out.println("Page title is: " + title);
+        Assert.assertTrue(title.contains("Google"), "Title does not match!");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
